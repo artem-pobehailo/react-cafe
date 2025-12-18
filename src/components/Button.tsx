fd5412b21fb31c768ef9576d2dbd09028022bb89
ARTEM POBEHAILO <partemishe@gmail.com>
2025-12-18 14:11:18 +0200

    Initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+// src/components/Button.tsx
+
+import clsx from "clsx";
+import css from "./Button.module.css";
+
+interface ButtonProps {
+  variant?: "primary" | "secondary";
+  text: string;
+  onClick?: () => void;
+}
+
+export default function Button({ variant, text, onClick }: ButtonProps) {
+  return (
+    <button
+      className={clsx(css.button, variant && css[variant])}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
